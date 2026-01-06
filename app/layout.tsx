996448d55cd5d18351d4682acf2e44b3af281f59
rushrealty <48533999+rushrealty@users.noreply.github.com>
2026-01-05 22:51:49 -0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -61,14 +61,9 @@ export default function RootLayout({
     <html lang="en" className="scroll-smooth">
       <head>
         <Script
-          id="google-maps-loader"
-          strategy="beforeInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `(g=>{var h,a,k,p="The Google Maps JavaScript API",c="google",l="importLibrary",q="__ib__",m=document,b=window;b[c]||(b[c]={});var d=b.maps||(b.maps={}),r=new Set,e=new URLSearchParams,u=()=>h||(h=new Promise(async(f,n)=>{await (a=m.createElement("script"));e.set("libraries",[...r]+"");for(k in g)e.set(k.replace(/[A-Z]/g,t=>"_"+t[0].toLowerCase()),g[k]);e.set("callback",c+".maps."+q);a.src=\`https://maps.\${c}apis.com/maps/api/js?\`+e;d[q]=f;a.onerror=()=>h=n(Error(p+" could not load."));a.nonce=m.querySelector("script[nonce]")?.nonce||"";m.head.append(a)}));d[l]?console.warn(p+" only loads once. Re-referencing: "+l):d[l]=(f,...n)=>r.add(f)&&u().then(()=>d[l](f,...n))})({
-              key: "AIzaSyCr7oXHFPoN5UsFynxNcR6w_G2YfJ-FE2w",
-              v: "weekly",
-            });`
-          }}
+  id="google-maps"
+  src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCr7oXHFPoN5UsFynxNcR6w_G2YfJ-FE2w&libraries=places,marker&v=weekly"
+  strategy="beforeInteractive"
         />
         <Script id="fub-pixel" strategy="afterInteractive">
           {`
